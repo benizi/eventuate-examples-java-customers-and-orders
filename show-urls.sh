7a318f7b95143012ea91b325f21071363b86b94b
Benjamin R. Haskell <java@benizi.com>
2021-07-07 12:43:16 -0400

    Use `DOCKER_HOST_IP` in URLs, too

--- show-urls.sh
+++ show-urls.sh
@@ -1,9 +1,11 @@
 #! /bin/bash -e
 
-./wait-for-services.sh ${DOCKER_HOST_IP:localhost} actuator/health 8081 8082 8083
+localhost=${DOCKER_HOST_IP:localhost}
+
+./wait-for-services.sh ${localhost} actuator/health 8081 8082 8083
 
 echo The microservices are running
 echo You can visit these URLS
-echo http://localhost:8081/swagger-ui/ - Customer service
-echo http://localhost:8083/swagger-ui/ - Order Service
-echo http://localhost:8082/swagger-ui/ - Order History Service
+echo http://${localhost}:8081/swagger-ui/ - Customer service
+echo http://${localhost}:8083/swagger-ui/ - Order Service
+echo http://${localhost}:8082/swagger-ui/ - Order History Service
